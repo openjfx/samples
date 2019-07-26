b2de58c1360c61a80472256d5ab7113995e3ef0e
jose.pereda <jose.pereda@gluonhq.com>
2019-07-26 20:59:01 +0200

    #107 Update samples with latest versions

--- CommandLine/Modular/Maven/hellofx/src/main/java/org/openjfx/MainApp.java
+++ CommandLine/Modular/Maven/hellofx/src/main/java/org/openjfx/MainApp.java
@@ -22,14 +22,6 @@ public class MainApp extends Application {
         stage.show();
     }
 
-    /**
-     * The main() method is ignored in correctly deployed JavaFX application.
-     * main() serves only as fallback in case the application can not be
-     * launched through deployment artifacts, e.g., in IDEs with limited FX
-     * support. NetBeans ignores main().
-     *
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         launch(args);
     }
