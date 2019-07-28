faf9a64a06f65c5e232a97461ca9cba35a30307a
jose.pereda <jose.pereda@gluonhq.com>
2019-07-28 21:42:00 +0200

    Update samples to latest JavaFX version

--- IDE/Eclipse/Modular/Java/HelloFX/src/org/openjfx/MainApp.java
+++ IDE/Eclipse/Modular/Java/HelloFX/src/org/openjfx/MainApp.java
@@ -1,7 +1,6 @@
 package org.openjfx;
 
 import javafx.application.Application;
-import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
